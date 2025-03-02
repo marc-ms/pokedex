6c2d3367bb6c36e0bbed739147dba6bc1f50f914
marc-ms <myphysics28@gmail.com>
2025-03-02 13:26:26 +0100

    fetching improvements

--- fetch_script.js
+++ fetch_script.js
@@ -1,20 +1,31 @@
 // variables
-//const pokemonContainer = document.getElementById("pokemon-list");
+const pokemonContainer = document.getElementById("pokemon-list");
 let pokemon = [];
 
 
 //functions
 
-const fetchPokemon = async () => {
-    console.log('init fetch pokemon')
+const fetchPokemon = async () => { // asynchronous function await
+    console.log('init fetch pokemon');
 
-    const url = "https://pokeapi.co:443/api/v2/pokemon?limit=151";
-    const response = await fetch(url);
-    const data = await response.json();
-    for (let i = 0; i < data['results'].length; i++){
-        console.log(data['results'][i]);
+    try {
+        const url = "https://pokeapi.co:443/api/v2/pokemon?limit=151";
+        const response = await fetch(url); // default option GET
+        const data = await response.json();
+        pokemon = data["results"];
+        console.log(pokemon);
+    } catch (error) {
+        console.log("Failed to get pokemon", error);
+        pokemonContainer.innerHTML = "<li>Failed to load</li>"; //html embedding
+        return; // if anything fails, stope here
     }
-    console.log(data['results'][1])
+
+    let template = "";
+    pokemon.forEach((iPokemon, index) => {
+        template += `<li>${index + 1} ${iPokemon.name}</li>`;
+    });
+
+    pokemonContainer.innerHTML = template;
 }
 
 fetchPokemon();
