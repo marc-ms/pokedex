3564f5c4757661f001c247fb3c3ccf1d8ec105b7
marc-ms <myphysics28@gmail.com>
2025-03-05 01:11:26 +0100

    icon and color by types added

--- fetch_script.js
+++ fetch_script.js
@@ -1,26 +1,6 @@
-//functions
-
-/* const fetchPokemons = async () => { // asynchronous function await
-    let pokemones = [];
-    try {
-        const url = "https://pokeapi.co:443/api/v2/pokemon?limit=151";
-        const response = await fetch(url); // default option GET
-        const data = await response.json();
-        pokemones = data["results"];
-        return pokemones;
-    } catch (error) {
-        console.log("Failed to get pokemon", error);
-        allPokemonContainer.innerHTML = "<li>Failed to load</li>"; //html embedding
-        return; // if anything fails, stope here
-    }
-
-   pokemones.forEach(function(pokemon){
-        fetchPokemonDetails(pokemon)
-    })
-
-} */
 
 async function getPokemon(startIndex, stopIndex){
+    console.log('inicio')
     let requests = [];
     for (let i = startIndex; i <= stopIndex; i++) {
         requests.push(apiPokemon("https://pokeapi.co/api/v2/pokemon/" + i));
@@ -28,6 +8,7 @@ async function getPokemon(startIndex, stopIndex){
 
     let pokemonList = await Promise.all(requests); // async to get pokemon in order
     for (let pokemon of pokemonList) {
+        console.log(pokemon)
         renderPokemon(pokemon);
     }
 }
@@ -39,21 +20,12 @@ async function apiPokemon(url){
     return pokemonData;
 }
 
-/* function fetchPokemonDetails(pokemon) {
-    let url = pokemon.url;
-    fetch(url)
-    .then(response => response.json())
-    .then(function(pokeData) {
-        console.log(pokeData)
-        renderPokemon(pokeData)
-    })
-} */
-
 function renderPokemon(pokeData) {
     const allPokemonContainer = document.getElementById("root");
     let pokeContainer = document.createElement('div');
 
     let pokeSprites = document.createElement('img')
+    pokeSprites.classList.add('sprites_img')
     pokeSprites.srcset = pokeData.sprites['front_default']
 
     let pokeName = document.createElement('h4')
@@ -64,21 +36,29 @@ function renderPokemon(pokeData) {
     pokeNumber.classList.add('id')
     pokeNumber.innerText = `#${pokeData.id}`
 
-    let pokeTypes = document.createElement('ul')
-    pokeTypes.classList.add('types')
-    createTypes(pokeData.types, pokeTypes)
+    let pokeTypes = document.createElement('div')
+    pokeTypes.classList.add('div_types')
+    createTypes(pokeData.types, pokeTypes, pokeContainer)
+    
 
     pokeContainer.append(pokeSprites, pokeName, pokeNumber, pokeTypes);
     allPokemonContainer.appendChild(pokeContainer);
 
 }
 
-function createTypes(types, ul) {
+function createTypes(types, div, pokeContainer) {
     types.forEach( function(type) {
-        let typeLi = document.createElement('li');
-        typeLi.innerText = type['type']['name'];
-        ul.append(typeLi)
+        let typeName = type['type']['name'];
+        let typeDiv = document.createElement('div');
+        let typeIcon = document.createElement('img');
+
+        typeDiv.classList.add('poke__type__icon', typeName);
+        typeIcon.srcset = "icons/" + typeName + ".svg";
+
+        div.append(typeDiv)
+        typeDiv.append(typeIcon)
     })
+    pokeContainer.classList.add(types[0]['type']['name']);
 }
 
 getPokemon(1, 151);
